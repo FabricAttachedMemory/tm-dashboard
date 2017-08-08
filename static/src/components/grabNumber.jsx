9507165059f088a2a5fa4121659e45c081ee2dab
lilly-wilson <lilly.wilson@hpe.com>
2017-08-08 13:05:12 -0600

    number looper

---
+++ static/src/components/grabNumber.jsx
@@ -0,0 +1,12 @@
+'use strict';
+import React from 'react';
+import {render} from 'react-dom';
+
+class GrabNumber extends React.Component{
+  grabnumber() {
+    const number = [0,1,2,3,4,5,6,7,8,9,15,32,44,48,50,66,73,81,97,100];
+    return number
+  }
+}
+
+export default GrabNumber;
